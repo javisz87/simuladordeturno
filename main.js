133f2eb9d2c190bf079056056c167e0542d86cc5
Javier <javiszumilo@hotmail.com>
2023-08-14 19:35:49 -0300

    Agregando DOM

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
+// Declarar el arreglo de turnos como una variable global
 const turnos = [];
- function sacarTurno(event) {
-  event.preventDefault(); // Evita que se recargue la página al enviar el formulario
-   const nombre = document.getElementById("nombre").value;
+ // Cargar datos almacenados en localStorage al cargar la página
+window.addEventListener("load", function() {
+  const turnosGuardados = localStorage.getItem("turnos");
+  if (turnosGuardados) {
+    turnos = JSON.parse(turnosGuardados);
+    mostrarTurnos();
+  }
+});
+ // Función para mostrar los turnos en el DOM
+function mostrarTurnos() {
+  const resultado = document.getElementById("resultado");
+  resultado.innerHTML = "";
+  turnos.forEach(function(turno, index) {
+    const numeroTurno = index + 1;
+    const turnoHTML = document.createElement("p");
+    turnoHTML.textContent = `Turno ${numeroTurno}: ${turno.nombre} ${turno.apellido} - ${turno.medico} - ${turno.fecha}`;
+    resultado.appendChild(turnoHTML);
+  });
+}
+ // Función para guardar los turnos en localStorage
+function guardarTurnos() {
+  localStorage.setItem("turnos", JSON.stringify(turnos));
+}
+ // Función para sacar un turno
+function sacarTurno(event) {
+  event.preventDefault();
+  const nombre = document.getElementById("nombre").value;
   const apellido = document.getElementById("apellido").value;
   const medicoSelect = document.getElementById("medico");
   const medico = medicoSelect.options[medicoSelect.selectedIndex].text;
@@ -9,8 +34,8 @@ const turnos = [];
    if (nombre && apellido && medico && fecha) {
     const numeroTurno = turnos.length + 1;
     turnos.push({ nombre, apellido, medico, fecha });
-     const resultado = document.getElementById("resultado");
-    resultado.innerHTML = `Turno ${numeroTurno}: ${nombre} ${apellido} - ${medico} - ${fecha}`;
+    mostrarTurnos();
+    guardarTurnos();
   } else {
     alert("Por favor, completa todos los campos.");
   }
